Fixed rotation when left set is rotated

main.py
@@ -34,10 +34,13 @@ def rotate_set(left_clf, left_set, right_clf, right_set, primary_support):
 
     returns a merged and rotated set, touple (X, y)
     """
-    
+
     # Get margins
     right_margin = get_margin(right_clf)
     left_margin = get_margin(left_clf)
+
+    #print("Left margin:     {}".format(left_margin))
+    #print("Right margin:    {}".format(right_margin))
 
     # intersection data
     intersection_point, angle = get_intersection_point(left_clf, right_clf)
@@ -47,7 +50,7 @@ def rotate_set(left_clf, left_set, right_clf, right_set, primary_support):
                             for point in right_set[0]]
 
     elif (left_margin > right_margin):
-        left_set[0] = [rotate_point(point, angle, primary_support, intersection_point)
+        left_set[0] = [rotate_point(point, -angle, primary_support, intersection_point)
                             for point in left_set[0]]
 
     else:
@@ -102,17 +105,6 @@ def ordering_support(vectors, point, clf):
     """
     primary_support_vector = None
 
-
-    # As the problem is binary classification, we will only have keys 0, 1
-    """
-    if (len(vectors[0]) is 1):
-        if (len(vectors[1]) > 1):
-            return 0
-
-    if (len(vectors[1]) is 1):
-        if (len(vectors[0]) > 1):
-            return 1
-    """
 
     w = clf.coef_[0]
 
@@ -213,15 +205,17 @@ def plot_clf(clf, ax, XX, YY, colour='k'):
            linestyles=['--', '-', '--'])
 
 
-def plot(new_clf, old_clf, X, y):
+def plot(new_clf, old_clf, X, y, splitting_point):
     """
     God function that removes all the jitter from main
     """
 
     plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
     ax = plt.gca()
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
+    xlim = (2, 10)
+    ylim = (-2, -11)
+
+    plt.axvline(x=splitting_point, color='k')
 
     # create grid to evaluate model
     xx = np.linspace(xlim[0], xlim[1], 30)
@@ -230,14 +224,15 @@ def plot(new_clf, old_clf, X, y):
     YY, XX = np.meshgrid(yy, xx)
 
 
-    plot_clf(new_clf, ax, XX, YY, 'b')
-    plot_clf(old_clf, ax, XX, YY, 'r')
+    plot_clf(new_clf, ax, XX, YY, 'g')
+    plot_clf(old_clf, ax, XX, YY, 'k')
 
     plt.show()
 
 def main():
     # Dataset
-    X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+
+    X, y = make_blobs(n_samples=165, centers=2, random_state=6)
 
     # Original SVM
     old_clf = svm.SVC(kernel='linear', C=1000)
@@ -249,7 +244,7 @@ def main():
     # Train on inital data
     old_clf.fit(X, y)
 
-    print("Old margin {}".format(get_margin(old_clf)))
+    old_margin = get_margin(old_clf)
 
 
     # Orginal support vectors
@@ -257,6 +252,9 @@ def main():
 
     # Splitting point
     primary_support = get_splitting_point(support_dict, old_clf)
+
+    # Used for plotting where the split occoured
+    splitting_point = primary_support[0]
 
     # Subsets of datasets, left and right of primary support vector
     left_set, right_set = split_data(primary_support, X, y)
@@ -267,21 +265,34 @@ def main():
 
 
     # Rotate and merge data sets back into one
+    old_X = X
+    old_y = y
     X, y = rotate_set(left_clf, left_set, right_clf, right_set, primary_support)
 
     # merge
     new_clf = svm.SVC(kernel='linear', C=1000)
     new_clf.fit(X, y)
 
-    print("New margin {}".format(get_margin(new_clf)))
+    new_margin = get_margin(new_clf)
 
     # Used for highlighting the sets
     right_set[0] = np.vstack(right_set[0])
     left_set[0] = np.vstack(left_set[0])
 
+    if (new_margin < old_margin):
+        print("WARNING: NEW IS LESS THAN OLD")
+        print
+
+        print("{}".format(new_margin - old_margin))
+    else:
+        print("{}".format(new_margin - old_margin))
+
     # plot new clf (post hyperplane folding) and old clf.
     # Blue is old, red is new.
-    plot(new_clf, old_clf, X, y)
+    plot(new_clf, old_clf, X, y, splitting_point)
+
+
+    plot(left_clf, right_clf, old_X, old_y, splitting_point)
 
 
 if __name__ == "__main__":