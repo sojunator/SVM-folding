Corrected error with max nr of folds. Test using dataset, ,how ever incorrect results are yielded.

hpf.py
@@ -234,11 +234,7 @@ class HPF:
         left_clf.fit(left_set[0], left_set[1])
 
         # Rotate and merge data sets back into one
-        self.data_points, self.data_labels, left_or_right, intersection_point = self.rotate_set(left_clf,
-                                                                                left_set,
-                                                                                right_clf,
-                                                                                right_set,
-                                                                                primary_support_vector)
+        self.data_points, self.data_labels, left_or_right, intersection_point = self.rotate_set(left_clf, left_set, right_clf, right_set, primary_support_vector)
 
         self.rotation_data.append((intersection_point, primary_support_vector, left_or_right, (right_clf, left_clf)))
 
@@ -540,7 +536,7 @@ class HPF:
             max_key = max(self.support_vectors_dictionary, key= lambda x: len(self.support_vectors_dictionary[x]))
         
             #get the direction between the two support vectors, and removes one of them from the dictionary
-            direction, self.support_vectors_dictionary[max_key] = self.get_direction_between_two_vectors_in_set_with_smallest_distance(self.support_vectors_dictionary[max_key], rotDim)
+            direction, self.support_vectors_dictionary[max_key] = self.get_direction_between_two_vectors_in_set_with_smallest_distance(self.support_vectors_dictionary[max_key], nr_of_coordinates)
         
             #calculate alignment matrix
             rotation_matrix = self.align_direction_matrix(direction)
@@ -654,14 +650,14 @@ class HPF:
 
         #fold until just two support vectors exist or max_nr_of_folds is reached
         current_fold = 0
-        while(len(self.clf.support_vectors_) > 2 or current_fold >= self.max_nr_of_folds):
+        while(len(self.clf.support_vectors_) > 2 and current_fold < self.max_nr_of_folds):
                 self.fold()
                 self.new_margin = self.get_margin(self.clf)
                 current_fold += 1
 
         stopper = 0
     
-    def __init__(self, rot_func = lambda p, i, r : np.matmul(p.T - i, r) + i, max_nr_of_folds = 99999):
+    def __init__(self, rot_func = lambda p, i, r : np.matmul(p.T - i, r) + i, max_nr_of_folds = 1):
         
         self.max_nr_of_folds = max_nr_of_folds
         self.clf = svm.SVC(kernel='linear', C=1000)

hpf_tester.py
@@ -11,24 +11,27 @@ from hpf_helpers import plot, read_data_from_folder
 from hpf import HPF
 
 
-print(read_data_from_folder("datasets"))
+#print(read_data_from_folder("datasets"))
 
-X, y = make_blobs(n_samples=40,
-                                     n_features=2,
-                                     centers=2,
-                                     random_state=6)
+#X, y = make_blobs(n_samples=40, n_features=2, centers=2, random_state=6)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 #test = HPF(X_train, y_train)
 data_points = np.array([[2.,7.,0.,2.,2.],[1.,5.,1.,2.,2.], [-1.,5.,2.,2.,2.], [-2.,3.,4.,5.,2.], [1.,2.,4.,5.,2.], [0.,0.,5.,5.,2.], [0.,4.,-1.,2.,2.], [0.,1.,5.,5.,2.]])
-data_labels = np.array([1,1,1,0,0,0,1,0])
+#data_labels = np.array([1,1,1,0,0,0,1,0])
 
-en_helt_ny_clf = HPF()
+data_set = read_data_from_folder("datasets")
+bmi_data_points, bmi_data_labels = data_set["bmi.csv"]
 
-en_helt_ny_clf.fit(data_points, data_labels)
+X_train, X_test, Y_train, Y_test = train_test_split(bmi_data_points, bmi_data_labels, test_size =0.33, random_state=42)
 
-print(en_helt_ny_clf.classify(np.array([[0.,2.,6.,5.,2.], [1.,6.,1.5,2.,2.],[0.,1.,5.,5.,2.]]))) #[0.,2.,6.,5.,2.]
+hpf = HPF()
+
+hpf.fit(X_train, Y_train)
+
+print("\nHPF GIVEN ANSWER: ", hpf.classify(X_test)) #[0.,2.,6.,5.,2.]
+print("\nEXPECTED  ANSWER: ", Y_test)
 
 
 # Ideally, resulst should be the same, but they ain't due to overotation