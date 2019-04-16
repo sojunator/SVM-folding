20696744aed99027939ef476200f43420d222b5d
Carl Söyseth <carlsoyseth@gmail.com>
2019-04-16 14:04:36 +0200

    Whatever

--- hpf_tester.py
+++ hpf_tester.py
@@ -30,11 +30,7 @@ X_train, Y_train = data_set["dimred.csv"]
 X_test = X_train[-5:]
 Y_test = Y_train[-5:]
 
-<<<<<<< HEAD
-#X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.5, random_state=42)
-=======
 #X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.75, random_state=42)
->>>>>>> 47a4a8f62e96db825fcbfaf7ba22e66d7828f4be
 #X_test, Y_test = np.array([[80, 60, 155]]), np.array([0])
 #X_test, Y_test = np.array([[0.0, 0.0, -4.0, 0.0]]), np.array([0])
 
